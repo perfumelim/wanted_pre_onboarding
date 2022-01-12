196e11674b44d72c5525571a236b5b6c41616ba2
perfumelim <perfumelim@gmail.com>
2022-01-12 15:17:05 +0900

    ADD: reset css 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
 import Navbar from "./components/navbar";
 import Main from "./pages/main";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
