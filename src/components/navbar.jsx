979d5d7fd9345a4c03888fed93ea55eeba4113f1
perfumelim <perfumelim@gmail.com>
2022-01-13 23:54:12 +0900

    ADD : img 경로 수정 및 nav 컴포넌트 구성

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 export default function navbar() {
   return (
     <NavContainer>
+      <image src={"/img/icon-menu.png"} />
+      <i className="icon-logo_new" />
       <ul>
         <li>채용</li>
         <li>이벤트</li>
@@ -13,12 +15,56 @@ export default function navbar() {
         <li>프리랜서</li>
         <li>AI 합격예측</li>
       </ul>
+      <AsideContainer>
+        <button>회원가입 / 로그인</button>
+        <DashboardButton>기업 서비스</DashboardButton>
+      </AsideContainer>
     </NavContainer>
   );
 }
 
 const NavContainer = styled.div`
+  display: flex;
+  justify-content: center;
   width: 100vw;
-  height: 5vh;
-  border-bottom: 1px solid #333333;
+  box-shadow: 0 1px 0 0 rgb(0 0 0 / 10%);
+
+  image {
+    width: 3vw;
+  }
+
+  ul {
+    display: flex;
+    font-size: 14px;
+    line-height: 20px;
+    font-weight: 600;
+
+    li {
+      padding: 15px;
+    }
+  }
+`;
+
+const AsideContainer = styled.div`
+  padding: 10px 0;
+
+  button {
+    padding: 0 3px;
+    font-size: 14px;
+    color: #333;
+    font-weight: 600;
+    line-height: 1;
+}
+  }
+  `;
+
+const DashboardButton = styled.button`
+  font-size: 13px;
+  color: #666;
+  line-height: 30px;
+  height: 30px;
+  border: 1px solid #e1e2e3;
+  border-radius: 15px;
+  padding: 0 10px;
+  font-weight: 400;
 `;
