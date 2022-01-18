174302c247c96f775c137163f2b6bf039c319e38
perfumelim <perfumelim@gmail.com>
2022-01-18 12:46:08 +0900

    ADD: Icon component 추가

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import SearchIcon from "../styles/SearchIcon";
+import AlarmIcon from "../styles/AlarmIcon";
 
 const MenuIcon = "images/icon-menu.png";
 
 export default function navbar() {
   return (
     <NavContainer>
-      <IconBar>
-        <Hamburger src={MenuIcon} />
-        <i className="icon-logo_new" />
-      </IconBar>
-      <ul>
-        <li>채용</li>
-        <li>이벤트</li>
-        <li>직군별 연봉</li>
-        <li>이력서 </li>
-        <li data-badge="New">커뮤니티</li>
-        <li>프리랜서</li>
-        <li data-badge="Beta">AI 합격예측</li>
-      </ul>
-      <AsideContainer>
-        <LoginBtn>회원가입/로그인</LoginBtn>
-        <BusinessWrapper>
-          <button>기업 서비스</button>
-        </BusinessWrapper>
-      </AsideContainer>
+      <NavWrapper>
+        <IconBar>
+          <Hamburger src={MenuIcon} />
+          <i className="icon-logo_new" />
+        </IconBar>
+        <ul>
+          <li>채용</li>
+          <li>이벤트</li>
+          <li>직군별 연봉</li>
+          <li>이력서 </li>
+          <li data-badge="New">커뮤니티</li>
+          <li>프리랜서</li>
+          <li data-badge="Beta">AI 합격예측</li>
+        </ul>
+        <AsideContainer>
+          <SearchIcon />
+          <AlarmIcon />
+          <BusinessWrapper>
+            <button>기업 서비스</button>
+          </BusinessWrapper>
+        </AsideContainer>
+      </NavWrapper>
     </NavContainer>
   );
 }
 
-const NavContainer = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+const NavContainer = styled.div`
   width: 100vw;
   box-shadow: 0 1px 0 0 rgb(0 0 0 / 10%);
 
@@ -70,6 +72,14 @@ const NavContainer = styled.nav`
   }
 `;
 
+const NavWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1060px;
+  margin: 0 auto;
+`;
+
 const IconBar = styled.div`
   display: flex;
 `;
@@ -84,14 +94,6 @@ const Hamburger = styled.img`
 const AsideContainer = styled.div`
   display: flex;
   padding: 10px 0;
-`;
-
-const LoginBtn = styled.button`
-  padding: 0 3px;
-  font-size: 14px;
-  color: #333;
-  font-weight: 500;
-  line-height: 1;
 `;
 
 const BusinessWrapper = styled.div`
